ccb51a13ca54c144ed2f3ec55310b7031303eed9
Rahul Shinde <rahulshinde.s2503@gmail.com>
2026-09-25 20:59:47 +0530

    Add application error handling

--- app.py
+++ app.py
@@ -872,6 +872,17 @@ def build_trek_from_form(trek):
 
 app = create_app()
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("errors/404.html"), 404
+
+@app.errorhandler(403)
+def access_forbidden(error):
+    return render_template("errors/403.html"), 403
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template("errors/500.html"), 500
 
 if __name__ == "__main__":
     app.run(debug=os.getenv("FLASK_DEBUG", "false").lower() == "true")
